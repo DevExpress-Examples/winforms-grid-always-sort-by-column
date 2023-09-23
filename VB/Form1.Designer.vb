fbb100dff53c62ddc4e391e3f3f6d56a7a728305
DevExpressExampleBot <fakeEmail>
2023-09-23 11:16:03 +0400

    Source auto update [skip ci]

--- VB/Form1.Designer.vb
+++ VB/Form1.Designer.vb
@@ -1,100 +1,100 @@
-Imports Microsoft.VisualBasic
-Imports System
 Namespace WindowsApplication165
-	Partial Public Class Form1
-		''' <summary>
-		''' Required designer variable.
-		''' </summary>
-		Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Clean up any resources being used.
-		''' </summary>
-		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-			If disposing AndAlso (components IsNot Nothing) Then
-				components.Dispose()
-			End If
-			MyBase.Dispose(disposing)
-		End Sub
+    Partial Class Form1
 
-		#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Required method for Designer support - do not modify
-		''' the contents of this method with the code editor.
-		''' </summary>
-		Private Sub InitializeComponent()
-			Me.gridControl1 = New DevExpress.XtraGrid.GridControl()
-			Me.gridView1 = New DevExpress.XtraGrid.Views.Grid.GridView()
-			Me.panelControl1 = New DevExpress.XtraEditors.PanelControl()
-			Me.checkEdit1 = New DevExpress.XtraEditors.CheckEdit()
-			CType(Me.gridControl1, System.ComponentModel.ISupportInitialize).BeginInit()
-			CType(Me.gridView1, System.ComponentModel.ISupportInitialize).BeginInit()
-			CType(Me.panelControl1, System.ComponentModel.ISupportInitialize).BeginInit()
-			Me.panelControl1.SuspendLayout()
-			CType(Me.checkEdit1.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
-			Me.SuspendLayout()
-			' 
-			' gridControl1
-			' 
-			Me.gridControl1.Dock = System.Windows.Forms.DockStyle.Fill
-			Me.gridControl1.EmbeddedNavigator.Name = ""
-			Me.gridControl1.Location = New System.Drawing.Point(0, 28)
-			Me.gridControl1.MainView = Me.gridView1
-			Me.gridControl1.Name = "gridControl1"
-			Me.gridControl1.ShowOnlyPredefinedDetails = True
-			Me.gridControl1.Size = New System.Drawing.Size(800, 477)
-			Me.gridControl1.TabIndex = 0
-			Me.gridControl1.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() { Me.gridView1})
-			' 
-			' gridView1
-			' 
-			Me.gridView1.GridControl = Me.gridControl1
-			Me.gridView1.Name = "gridView1"
-			' 
-			' panelControl1
-			' 
-			Me.panelControl1.Controls.Add(Me.checkEdit1)
-			Me.panelControl1.Dock = System.Windows.Forms.DockStyle.Top
-			Me.panelControl1.Location = New System.Drawing.Point(0, 0)
-			Me.panelControl1.Name = "panelControl1"
-			Me.panelControl1.Size = New System.Drawing.Size(800, 28)
-			Me.panelControl1.TabIndex = 1
-			' 
-			' checkEdit1
-			' 
-			Me.checkEdit1.EditValue = True
-			Me.checkEdit1.Location = New System.Drawing.Point(5, 5)
-			Me.checkEdit1.Name = "checkEdit1"
-			Me.checkEdit1.Properties.Caption = "sort by ""EmployeeID"" column after/before defined sorting"
-			Me.checkEdit1.Size = New System.Drawing.Size(410, 19)
-			Me.checkEdit1.TabIndex = 0
-			' 
-			' Form1
-			' 
-			Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
-			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-			Me.ClientSize = New System.Drawing.Size(800, 505)
-			Me.Controls.Add(Me.gridControl1)
-			Me.Controls.Add(Me.panelControl1)
-			Me.Name = "Form1"
-			Me.Text = "Form1"
-			CType(Me.gridControl1, System.ComponentModel.ISupportInitialize).EndInit()
-			CType(Me.gridView1, System.ComponentModel.ISupportInitialize).EndInit()
-			CType(Me.panelControl1, System.ComponentModel.ISupportInitialize).EndInit()
-			Me.panelControl1.ResumeLayout(False)
-			CType(Me.checkEdit1.Properties, System.ComponentModel.ISupportInitialize).EndInit()
-			Me.ResumeLayout(False)
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
+            End If
 
-		End Sub
+            MyBase.Dispose(disposing)
+        End Sub
 
-		#End Region
+#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Me.gridControl1 = New DevExpress.XtraGrid.GridControl()
+            Me.gridView1 = New DevExpress.XtraGrid.Views.Grid.GridView()
+            Me.panelControl1 = New DevExpress.XtraEditors.PanelControl()
+            Me.checkEdit1 = New DevExpress.XtraEditors.CheckEdit()
+            CType((Me.gridControl1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.gridView1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.panelControl1), System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.panelControl1.SuspendLayout()
+            CType((Me.checkEdit1.Properties), System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.SuspendLayout()
+            ' 
+            ' gridControl1
+            ' 
+            Me.gridControl1.Dock = System.Windows.Forms.DockStyle.Fill
+            Me.gridControl1.EmbeddedNavigator.Name = ""
+            Me.gridControl1.Location = New System.Drawing.Point(0, 28)
+            Me.gridControl1.MainView = Me.gridView1
+            Me.gridControl1.Name = "gridControl1"
+            Me.gridControl1.ShowOnlyPredefinedDetails = True
+            Me.gridControl1.Size = New System.Drawing.Size(800, 477)
+            Me.gridControl1.TabIndex = 0
+            Me.gridControl1.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() {Me.gridView1})
+            ' 
+            ' gridView1
+            ' 
+            Me.gridView1.GridControl = Me.gridControl1
+            Me.gridView1.Name = "gridView1"
+            ' 
+            ' panelControl1
+            ' 
+            Me.panelControl1.Controls.Add(Me.checkEdit1)
+            Me.panelControl1.Dock = System.Windows.Forms.DockStyle.Top
+            Me.panelControl1.Location = New System.Drawing.Point(0, 0)
+            Me.panelControl1.Name = "panelControl1"
+            Me.panelControl1.Size = New System.Drawing.Size(800, 28)
+            Me.panelControl1.TabIndex = 1
+            ' 
+            ' checkEdit1
+            ' 
+            Me.checkEdit1.EditValue = True
+            Me.checkEdit1.Location = New System.Drawing.Point(5, 5)
+            Me.checkEdit1.Name = "checkEdit1"
+            Me.checkEdit1.Properties.Caption = "sort by ""EmployeeID"" column after/before defined sorting"
+            Me.checkEdit1.Size = New System.Drawing.Size(410, 19)
+            Me.checkEdit1.TabIndex = 0
+            ' 
+            ' Form1
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(800, 505)
+            Me.Controls.Add(Me.gridControl1)
+            Me.Controls.Add(Me.panelControl1)
+            Me.Name = "Form1"
+            Me.Text = "Form1"
+            CType((Me.gridControl1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.gridView1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.panelControl1), System.ComponentModel.ISupportInitialize).EndInit()
+            Me.panelControl1.ResumeLayout(False)
+            CType((Me.checkEdit1.Properties), System.ComponentModel.ISupportInitialize).EndInit()
+            Me.ResumeLayout(False)
+        End Sub
 
-		Private gridControl1 As DevExpress.XtraGrid.GridControl
-		Private gridView1 As DevExpress.XtraGrid.Views.Grid.GridView
-		Private panelControl1 As DevExpress.XtraEditors.PanelControl
-		Private checkEdit1 As DevExpress.XtraEditors.CheckEdit
-	End Class
+#End Region
+        Private gridControl1 As DevExpress.XtraGrid.GridControl
+
+        Private gridView1 As DevExpress.XtraGrid.Views.Grid.GridView
+
+        Private panelControl1 As DevExpress.XtraEditors.PanelControl
+
+        Private checkEdit1 As DevExpress.XtraEditors.CheckEdit
+    End Class
 End Namespace
-
